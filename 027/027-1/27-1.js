a05ab8015807ac1815f43458a989a0532b782a5e
ShaqayeqAfshar <99327436+ShaqayeqAfshar@users.noreply.github.com>
2022-09-20 19:02:48 +0430

    Add files via upload

---
+++ 027/027-1/27-1.js
@@ -0,0 +1,6 @@
+
+
+let temper = prompt("Enter the temperature.");
+let result =(temper-32)*5/9;
+console.log(result);
+alert(result);
---
+++ 027/027-2/27-2.js
@@ -0,0 +1,7 @@
+
+let weight = prompt("Enter your weight(by kg)");
+let height = prompt("Enter your height (by m)");
+
+let result = weight / (height**2);
+console.log(result);
+alert(result)
